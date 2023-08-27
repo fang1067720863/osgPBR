16df7f679d9da8cae4829e00a5b1020db9ead271
fangyuanyuan <1067720863@qq.com>
2023-08-27 10:07:23 +0800

    #230827

---
+++ OE_PBR/AdvancedMaterial.cpp
@@ -0,0 +1,85 @@
+#include<Windows.h>
+#include"AdvancedMaterial.h"
+namespace osgEarth
+{
+	const std::string AdvancedMaterial::USE_IOR = std::string("USE_IOR");
+	const std::string AdvancedMaterial::USE_SHEEN = std::string("USE_SHEEN");
+	const std::string AdvancedMaterial::USE_CLEARCOAT = std::string("USE_CLEARCOAT");
+	const std::string AdvancedMaterial::USE_IRIDESCENCE = std::string("USE_IRIDESCENCE");
+	const std::string AdvancedMaterial::USE_TRANSMISSION = std::string("USE_TRANSMISSION");
+
+	void AdvancedMaterial::initTechniqueCallback()
+	{
+		auto sheenCB = [](osg::StateAttribute* attr, osg::NodeVisitor* nv)
+		{
+            osgEarth::AdvancedMaterial* material = static_cast<osgEarth::AdvancedMaterial*>(attr);
+			for (unsigned int i = 0; i < attr->getNumParents(); i++)
+			{
+				osg::StateSet* stateSet = attr->getParent(i);
+				auto on = material->getUseSheen() ? osg::StateAttribute::ON : osg::StateAttribute::OFF;
+				stateSet->setDefine(USE_SHEEN, "1", on);
+				if (!material->getUseSheen())
+				{
+					return;
+				}
+
+				static const std::string SHEENCOLOR = UPREFIX "sheenColor";
+				static const std::string SHEENROUGHNESS = UPREFIX "sheenRoughness";
+				stateSet->getOrCreateUniform(SHEENCOLOR, osg::Uniform::FLOAT_VEC3)->set(material->getSheenColor());
+				stateSet->getOrCreateUniform(SHEENROUGHNESS, osg::Uniform::FLOAT)->set(material->getSheenRoughness());
+
+				auto sheenColorMap = material->getSheenColorMap();
+				auto sheenRoughnessMap	= material->getSheenRoughnessMap();
+				if (sheenColorMap.valid())
+				{
+					//auto defineKey = textureDefines[TextureEnum::SHEENCOLORMAP];
+					stateSet->setTextureAttributeAndModes(material->texUnitCnt(), material->getSheenColorMap(), osg::StateAttribute::ON);
+					stateSet->getOrCreateUniform("SheenColorMap", osg::Uniform::SAMPLER_2D)->set(material->texUnitCnt());
+					material->incementTexUnit();
+
+				}
+				if (sheenRoughnessMap.valid())
+				{
+					stateSet->setTextureAttributeAndModes(material->texUnitCnt(), material->getSheenRoughnessMap(), osg::StateAttribute::ON);
+					stateSet->getOrCreateUniform("SheenRoughnessMap", osg::Uniform::SAMPLER_2D)->set(material->texUnitCnt());
+					material->incementTexUnit();
+				}	
+			}
+
+		};
+
+		auto clearCoatCB = [](osg::StateAttribute* attr, osg::NodeVisitor* nv)
+		{
+			osgEarth::AdvancedMaterial* material = static_cast<osgEarth::AdvancedMaterial*>(attr);
+			for (unsigned int i = 0; i < attr->getNumParents(); i++)
+			{
+				osg::StateSet* stateSet = attr->getParent(i);
+				auto on = material->getUseClearcoat() ? osg::StateAttribute::ON : osg::StateAttribute::OFF;
+				stateSet->setDefine(USE_CLEARCOAT, "1", on);
+				if (!material->getUseClearcoat())
+				{
+					return;
+				}
+			
+				stateSet->getOrCreateUniform(UPREFIX "clearcoat", osg::Uniform::FLOAT)->set(material->getClearcoat());
+				stateSet->getOrCreateUniform(UPREFIX "clearcoatRoughness", osg::Uniform::FLOAT)->set(material->getClearcoatRoughness());
+
+			}
+
+		};
+		
+		_techniqueCallbacks = new SAttrCallback();
+		_techniqueCallbacks->addUpdateCallback("sheen", sheenCB);
+		_techniqueCallbacks->addUpdateCallback("clearcoat", clearCoatCB);
+		setUpdateCallback(_techniqueCallbacks);
+	}
+	AdvancedMaterial::AdvancedMaterial():StandardPBRMaterial()
+	{
+		initTechniqueCallback();
+	}
+
+	void AdvancedMaterial::addUpdateCallback(const std::string& key, osg::StateAttributeCallback* f)
+	{
+		_techniqueCallbacks->addUpdateCallback(key, f);
+	}
+}
--- README.md
+++ README.md
@@ -8,3 +8,27 @@
  ## V1.1
 - 新增透明材质
 - 新增IBL环境光照
+- 扩展了dds插件对CubeMap的读取 和 GL_RGBA_ARB扩展压缩格式的支持
+- 【Tool】 cubemap -> quadMap
+
+ ## V1.2
+- 提供osgEarth::ExtensionedMaterial 可自定义材质
+- 实现了waterial example from ue
+- 提供osg自己的材质资产格式 .omat
+- 可以切换 HDR specularHDR 可设置mipmap
+- OPAQUE
+
+## V1.3
+- 提供osgEarth::AdvancedMaterial 
+- 支持sheen和clearcloat特性
+- gtlf插件增加对khr_extenison的支持
+- 完成relection probe cubeCamera以支持transmission
+
+# TODO
+- 直接通过mipmap Texture 写入到硬盘
+- 透明材质
+- 后处理
+- 高级材质
+
+- sheen tranmission ior anisotropy variants iridescence 
+- exposure
